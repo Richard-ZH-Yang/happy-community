47996407a129348e51d87b7a70fe9de253fd31e3
Zichun Hao <zichunh2000@gmail.com>
2021-08-07 16:38:13 -0700

    Fix bug

--- web/diary.js
+++ web/diary.js
@@ -37,7 +37,7 @@ function getScore(content) {
         xhttp.open("GET", `http://localhost:8080/login/diary/score?content=${diaryContent}`, true);
         xhttp.setRequestHeader("Content-Type", "application/json");
         xhttp.send(JSON.stringify({"content": content}));
-        xhttp.onload() = function() {
+        xhttp.onload = function() {
             score = this.responseText;
             console.log(`score = ${score}`);
             msg.classList.add('score');
