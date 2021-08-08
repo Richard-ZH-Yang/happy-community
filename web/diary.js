6560c051be91e223f79559d4114b6a6e47b54311
Zichun Hao <zichunh2000@gmail.com>
2021-08-07 17:52:19 -0700

    Fix

--- web/diary.js
+++ web/diary.js
@@ -39,7 +39,7 @@ function getScore(content) {
         xhttp.setRequestHeader("Content-Type", "application/json");
         // xhttp.send(JSON.stringify({"content": content}));
         xhttp.onload = function() {
-            score = parseInt(this.responseText['score']) * 100;
+            score = this.responseText['score'];
             console.log(`score = ${score}`);
             msg.classList.add('score');
             msg.innerHTML = `Score: ${score}`;
