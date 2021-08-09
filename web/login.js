db9442ddef5a1d7acf9b3e6ceb3235018fbcc41c
Zichun Hao <zichunh2000@gmail.com>
2021-08-09 00:51:00 -0700

    Login

--- web/login.js
+++ web/login.js
@@ -16,21 +16,51 @@ btn.addEventListener('mouseout', (e) => {
 
 const loginButton = document.getElementById("login-submit");
 const goBackButton = document.getElementById("go-to-index");
+const username = document.getElementById('username');
+const password = document.getElementById('password');
 const msg = document.querySelector('.msg');
 
 // login
-loginButton.addEventListener('click', onSubmit);
-function onSubmit(e) {
+loginButton.addEventListener('click', onLogin);
+function onLogin(e) {
     e.preventDefault();
     
     if (username.value === '' || password.value === '') {
         msg.classList.add('error');
         msg.innerHTML = 'Please enter all fields';
     } else {
-        location.href = 'diary.html';
+        getUserStatus();
     }
-
-
     
 }
 
+function getUserStatus() {
+    location.href = 'diary.html';
+}
+
+function getLoginStatus() {
+    try {
+        let xhttp = new XMLHttpRequest();
+        xhttp.open("GET", `http://localhost:8080/login/login?username=${username}&password=${password}`, true);
+        xhttp.setRequestHeader("Content-Type", "application/json");
+        xhttp.onload = function() {
+            const score = this.responseText;
+            updateLoginPage(score);
+        }
+        xhttp.send();
+    } catch (e) {
+        console.log(e);
+    } finally {
+        msg.classList.add('message');
+        msg.innerHTML = 'Connecting...';
+    }
+}
+
+function updateLoginPage(score) {
+    if (score > 0) {
+        location.href = 'diary.html';
+    } else {
+        msg.classList.add('error');
+        msg.innerHTML = 'Username or password is not correct. Please try again.';
+    }
+}
