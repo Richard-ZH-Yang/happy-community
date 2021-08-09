d73c9cfedacda820a8c1bb9cdc2f44922ff3ceb9
Shiyao-monster <shiyaochn@gmail.com>
2021-08-09 17:54:05 -0400

    final login

--- src/web/js/login.js
+++ src/web/js/login.js
@@ -3,48 +3,44 @@ const goBackButton = document.getElementById("go-to-index");
 const username = document.getElementById('username');
 const password = document.getElementById('password');
 const msg = document.querySelector('.msg');
-
 // login
-loginButton.addEventListener('click', onLogin);
-function onLogin(e) {
+loginButton.addEventListener('click', onSubmit);
+function onSubmit(e) {
     e.preventDefault();
     
     if (username.value === '' || password.value === '') {
         msg.classList.add('error');
         msg.innerHTML = 'Please enter all fields';
     } else {
-        getUserStatus();
+        var a = login()
+        console.log(a)
+
     }
+
+
     
 }
 
-function getUserStatus() {
-    location.href = 'diary.html';
-}
+function login() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
 
-function getLoginStatus() {
-    try {
-        let xhttp = new XMLHttpRequest();
-        xhttp.open("GET", `http://localhost:8080/login/login?username=${username}&password=${password}`, true);
-        xhttp.setRequestHeader("Content-Type", "application/json");
-        xhttp.onload = function() {
-            const score = this.responseText;
-            updateLoginPage(score);
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("GET", `http://localhost:8080/login/login?username=${username}&password=${password}`, true);
+    xhttp.onload = function(e){
+        if(this.status==200) {
+            var users = JSON.parse(this.responseText);
+            if (username == users['username']) {
+                console.log( users['username']);
+                location.href = `diary.html?username=${username}`;
+            } else{
+                console.log('wrong');
+                msg.classList.add('error');
+                msg.innerHTML = 'Something Wrong. Please try again';
+            }
         }
-        xhttp.send();
-    } catch (e) {
-        console.log(e);
-    } finally {
-        msg.classList.add('message');
-        msg.innerHTML = 'Connecting...';
     }
-}
+    xhttp.send();
+    
 
-function updateLoginPage(score) {
-    if (score > 0) {
-        location.href = 'diary.html';
-    } else {
-        msg.classList.add('error');
-        msg.innerHTML = 'Username or password is not correct. Please try again.';
-    }
 }
