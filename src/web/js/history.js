0f64434c5300efa7c03164f1c72006ab95daa5bd
Richard <richardyangzh@gmail.com>
2021-08-09 22:24:44 -0700

    history page

--- src/web/js/history.js
+++ src/web/js/history.js
@@ -5,13 +5,30 @@ window.addEventListener('load', (event) => {
 
 
 function loadHistory() {
+    var url = window.location.href;
     let xhttp = new XMLHttpRequest();
-    xhttp.open("GET", `http://localhost:8080/history/history?username=${username}`, true);
-    xhttp.onload = function(e){
-        if(this.status==200) {
-            var info = JSON.parse(this.responseText);
-
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 and this.status == 200) {
+            loadHistoryContent(this);
         }
-    }
+    };
+    xhttp.open("GET", url, true);
     xhttp.send();
+
+}
+
+
+function loadHistoryContent(xhttp) {
+    var info = JSON.parse(xhttp.responseText);
+    var newContent = "<div class='historyContent'>";
+    info.forEach(function (one) {
+        var score = one.score;
+        var one_content = one.content;
+        newContent += `<div class = "each">` +
+            `<h2> ${score} </h2>` +
+            `<p> ${one_content} </p>`+
+            `</div>`;
+    })
+    newContent += "</div>";
+    document.getElementById("history_content").innerHTML = newContent;
 }
