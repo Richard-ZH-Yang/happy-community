3fac20e9a69f79b6d3c861b98fb394a2bd087133
Shiyao-monster <shiyaochn@gmail.com>
2021-08-10 17:01:59 -0400

    history laod and share function finished

--- src/web/js/history.js
+++ src/web/js/history.js
@@ -1,5 +1,6 @@
 let i = 0;
 let buttons;
+const msg = document.querySelector('.msg');
 
 window.addEventListener('load', (event) => {
     loadHistory();
@@ -36,20 +37,42 @@ function loadHistory() {
 
 
 function loadHistoryContent(xhttp) {
+    var cur_url = window.location.href;
+    var params = (new URL(cur_url)).searchParams;
+    var user = params.get('username');
+
     var info = JSON.parse(xhttp.responseText);
     var newContent = "<div class='historyContent'>";
     info.forEach(function (one) {
         var one_content = one.content;
         var score = one.score;
         var time = one.time;
+        var content_id = one.content_id;
         newContent += `<div class = "each">` +
-            `<h2> ${score} </h2>` +
-            `<h3> ${time} </h3>` +
+            `<h3> Score: ${score} </h3>` +
+            `<h5> Diary ID: ${content_id} </h5>` +
+            `<h5> ${time} </h5>` +
             `<p> ${one_content} </p>`+
-            `<input class="btn" type="button" id="history-share-${i}" value="Share to community">`+
+            //`<input class="btn" type="button" id="history-share-${id}" value="Share to community">`+
+            `<button type="button" class="btn" onClick="shareDiary('${user}','${content_id}')">Share to community</button>` + 
             `</div>`;
         i++;
     })
     newContent += "</div>";
-    document.getElementById("history_content").innerHTML = newContent;
+    document.getElementById("my-form").innerHTML = newContent;
+}
+
+function shareDiary(username, content_id){
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("GET", `http://localhost:8080/login/diary/share?username=${username}&content_id=${content_id}`, true);
+    xhttp.onload = function() {
+        const score = this.responseText;
+        if (score == "-1") {
+            msg.classList.add('score');
+            msg.innerHTML = `Diary ID: ${content_id} already shared`;           
+      
+        }
+        
+    }
+    xhttp.send();
 }
