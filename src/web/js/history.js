fca88b51e1016b9734210aec4af8065958a5cfb7
Richard <richardyangzh@gmail.com>
2021-08-10 00:31:20 -0700

    reformat code and initialize buttons

--- src/web/js/history.js
+++ src/web/js/history.js
@@ -1,7 +1,21 @@
+let i = 0;
+let buttons;
+
 window.addEventListener('load', (event) => {
     loadHistory();
+    initializeButtons();
     console.log('The page has fully loaded');
 });
+
+
+function initializeButtons() {
+    buttons = [i + 1];
+    var j = i;
+    while (j >= 0) {
+        buttons.push(document.getElementById(`history-share-${j}`));
+        j--;
+    }
+}
 
 
 function loadHistory() {
@@ -12,7 +26,7 @@ function loadHistory() {
 
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            loadHistoryContent(this, user);
+            loadHistoryContent(this);
         }
     };
     xhttp.open("GET", `http://localhost:8080/login/diary/show?username=${user}`, true);
@@ -21,7 +35,7 @@ function loadHistory() {
 }
 
 
-function loadHistoryContent(xhttp, username) {
+function loadHistoryContent(xhttp) {
     var info = JSON.parse(xhttp.responseText);
     var newContent = "<div class='historyContent'>";
     info.forEach(function (one) {
@@ -32,8 +46,9 @@ function loadHistoryContent(xhttp, username) {
             `<h2> ${score} </h2>` +
             `<h3> ${time} </h3>` +
             `<p> ${one_content} </p>`+
-            `<input class="btn" type="button" id="history-share-${username}" value="Share to community">`+
+            `<input class="btn" type="button" id="history-share-${i}" value="Share to community">`+
             `</div>`;
+        i++;
     })
     newContent += "</div>";
     document.getElementById("history_content").innerHTML = newContent;
